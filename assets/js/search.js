701a5b382ebbe9abacfaa6063d825198ce3babb1
dorae222 <dorae222@users.noreply.github.com>
2022-11-16 09:29:26 +0000

    deploy: 4a48dff10679d4333fbe4854636744330ccd6d57

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/dorae222/assets/js/search-data.json', true);
+    request.open('GET', '/https://dorae222.github.io/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
